2a9bf2ece94aa069d911965e1c691975c7513d3d
Gino Ochoa <gochoa1088@gmail.com>
2022-05-24 12:04:21 -0700

    openssl https

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+const express = require("express");
 
 const PORT = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/secret", (req, res) => {
+  return res.send("Your personal secret value is 42!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
