2c93f1278c57ea3cd1b0884d4a01e7350f5fd81b
Gino Ochoa <gochoa1088@gmail.com>
2022-05-25 10:15:07 -0700

    setting up cookie session middleware

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const helmet = require("helmet");
 const express = require("express");
 const passport = require("passport");
 const { Strategy } = require("passport-google-oauth20");
+const { verify } = require("crypto");
+const cookieSession = require("cookie-session");
 
 require("dotenv").config();
 
@@ -12,6 +14,8 @@ const PORT = 3000;
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
@@ -30,6 +34,13 @@ passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 const app = express();
 
 app.use(helmet());
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
 app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
